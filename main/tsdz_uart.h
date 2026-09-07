74d67bfb4fed976e1be0822c6bede2aef4d2b502
placid888 <placid888@gmail.com>
2026-09-07 21:41:05 +0800

    update tsdz_uart.h

--- main/tsdz_uart.h
+++ main/tsdz_uart.h
@@ -13,10 +13,10 @@
 #define LCD_UART            UART_NUM_1
 #define CT_UART             UART_NUM_2
 
-#define CT_TX_PIN           GPIO_NUM_27  // Controller TX and RX2 for ESP32
-#define CT_RX_PIN           GPIO_NUM_25  // Controller RX and TX2 for ESP32
-#define LCD_TX_PIN          GPIO_NUM_26  // LCD TX and RX1 for ESP32
-#define LCD_RX_PIN          GPIO_NUM_33  // LCD RX and TX1 for ESP32
+#define CT_TX_PIN           GPIO_NUM_16  // Controller TX and RX2 for ESP32
+#define CT_RX_PIN           GPIO_NUM_17  // Controller RX and TX2 for ESP32
+#define LCD_TX_PIN          GPIO_NUM_22  // LCD TX and RX1 for ESP32
+#define LCD_RX_PIN          GPIO_NUM_23  // LCD RX and TX1 for ESP32
 
 #define CT_MSG_ID           0x43
 #define LCD_MSG_ID          0x59
